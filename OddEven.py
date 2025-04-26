d7656fda600158fc4a4e067a44b25d46a93fe830
Darren584 <darren.alweyi@strathmore.edu>
2025-04-26 21:41:07 +0300

    Create OddEven.py

---
+++ OddEven.py
@@ -0,0 +1,9 @@
+# Function to check if a number is even or odd
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"  # If divisible by 2, it's even
+    else:
+        return "Odd"   # Otherwise, it's odd
+
+# Example usage:
+# print(check_even_odd(7))  # Output: Odd

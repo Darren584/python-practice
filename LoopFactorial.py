23069c46fd18213ac703fb297d64288e7f027004
Darren584 <darren.alweyi@strathmore.edu>
2025-04-26 21:46:59 +0300

    LoopFactorial.py

--- LoopFactorial.py
+++ LoopFactorial.py
@@ -4,6 +4,3 @@ def factorial_loop(n):
     for i in range(1, n + 1):
         result *= i  # Multiply result by each number up to n
     return result  # Return the factorial
-
-# Example usage:
-# print(factorial_loop(5))  # Output: 120

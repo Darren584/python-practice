f752cbb5adcf929fad8c8d59287679a4a6bcdec0
Darren584 <darren.alweyi@strathmore.edu>
2025-04-26 21:46:36 +0300

    FactorialRecursive.py

--- FactorialRecursive.py
+++ FactorialRecursive.py
@@ -4,6 +4,3 @@ def factorial_recursive(n):
         return 1  # Base case: factorial of 0 or 1 is 1
     else:
         return n * factorial_recursive(n - 1)  # Recursive step
-
-# Example usage:
-# print(factorial_recursive(5))  # Output: 120
